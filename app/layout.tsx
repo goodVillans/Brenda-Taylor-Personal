ebf82a32e27793d661982dc284dae890cf311473
Troy <tempestbtroy@outlook.com>
2023-11-13 18:21:14 +0200

    resolve type error causing build fail

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,13 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import icon from './icon.png'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Brenda Taylor - Movement and Pilates specialist',
+  title: 'Brenda Taylor - movement and pilates specialist',
   description: 'Meet Brenda Taylor, a dedicated Pilates and movement specialist passionate about enhancing your physical wellness and flexibility. With a unique blend of expert Pilates instruction and tailored movement therapy, Brenda helps clients achieve balanced, pain-free living and improved body awareness. Discover your path to a stronger, more agile you with Brendas holistic approach to fitness and health.',
-  icon: icon
+  icons: './icon.png'
 }
 
 export default function RootLayout({
@@ -18,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`layout ${inter.className}`}>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
